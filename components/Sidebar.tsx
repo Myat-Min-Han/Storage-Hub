68c2baabf6088f81935429c5de7d4a026518799e
david <davidmyatminhan123@gmail.com>
2025-12-05 22:16:45 +0800

    added sidebar responsive

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,9 @@
-import { Box, LayoutDashboard, PackageSearch, Settings, ChartBarBig } from "lucide-react";
+"use client";
+
+import { Box, LayoutDashboard, PackageSearch, Settings, ChartBarBig, Menu } from "lucide-react";
 import Link from "next/link";
 import { UserButton } from "@stackframe/stack";
+import { useState } from "react";
 
 const navigations = [
     { name: "dashboard", href: "/dashboard", icon: <LayoutDashboard />},
@@ -10,8 +13,12 @@ const navigations = [
 ]
 
 export default function Sidebar() {
+
+    const [ isOpen, setIsOpen ] = useState<boolean>(false);
+    
     return (
-        <aside className="min-h-screen bg-gray-900 text-white w-56">
+        <>
+        <aside className="min-h-screen bg-gray-900 text-white sm:w-56 hidden md:block">
             <div className="flex gap-3 items-center p-4">
                 <Box className="w-10 h-10"/>
                 <h1 className="font-semibold text-xl">Storage Hub</h1>
@@ -28,5 +35,38 @@ export default function Sidebar() {
                 <UserButton showUserInfo/>
             </footer>
         </aside>
+        <div className="sm:hidden block p-4">
+            <button onClick={() => setIsOpen(true)} className="p-1 rounded-md hover:bg-gray-200 cursor-pointer">
+                <Menu className="w-6 h-6"/>
+            </button>
+        </div>
+        { isOpen && (
+            <aside className={`fixed top-0 left-0 h-full w-56 bg-gray-900 text-white transform 
+                ${isOpen ? "translate-x-0" : "-translate-x-full"} 
+                transition-transform duration-300 ease-in-out md:hidden z-50`}
+            >
+                <div className="flex gap-3 items-center p-4">
+                    <Box className="w-10 h-10" />
+                    <h1 className="font-semibold text-xl">Storage Hub</h1>
+                </div>
+                <nav className="flex flex-col gap-4 p-5">
+                    {navigations.map((navigation, index) => (
+                        <Link
+                            href={navigation.href}
+                            key={index}
+                            className="flex gap-4 hover:bg-blue-600 p-2 hover:rounded-md"
+                            onClick={() => setIsOpen(false)} 
+                        >
+                            {navigation.icon}
+                            {navigation.name}
+                        </Link>
+                    ))}
+                </nav>
+                <footer className="fixed bottom-0 p-5 ">
+                    <UserButton showUserInfo/>
+                </footer>
+            </aside>
+        )}
+        </>
     )
 }
